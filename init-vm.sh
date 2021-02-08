63f67effb69cba8fe713803a40c203ac97cff161
jonandernovella <novella.uu@gmail.com>
2021-02-08 15:50:05 +0100

    Fix up command

--- init-vm.sh
+++ init-vm.sh
@@ -7,6 +7,7 @@ pushd $project
 git clone https://github.com/neicnordic/sda-helm -b "master"
 git clone git@github.com:NBISweden/LocalEGA-SE-Deployment.git -b "master"
 
+popd
+
 vagrant up --provision
 
-popd
